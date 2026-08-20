8d878933481f1b28421b57d8160a1fcff9fafc73
simoncock101 <simoncock101@hotmail.com>
2026-08-20 10:49:35 -0500

    Update subheader text in app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ try:
 except:
     pass
 
-st.subheader("Una pequeña Fábula.")
+st.subheader("Ejemplo de texto que puedes usar")
 st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
          ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
          ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
